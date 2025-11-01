afed13f50321a6dc33ce26c9269ae9bbcdb85684
eKlerner04 <emilklerner@icloud.com>
2025-11-01 14:42:32 +0200

    Initial Commit

---
+++ SkatCalculator/SkatCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkatCalculatorApp.swift
+//  SkatCalculator
+//
+//  Created by Emil Klerner on 01.11.25.
+//
+
+import SwiftUI
+
+@main
+struct SkatCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
